2cdf22fcdc9b0577d09ecd301be6905dc22bfb49
stuart yeates <syeates@gmail.com>
2014-10-11 23:15:10 +1300

    first running code

--- src/CSVFormatDiviner.java
+++ src/CSVFormatDiviner.java
@@ -1,6 +1,8 @@
 /**
  * 
  */
+import org.apache.commons.csv.*;
+import java.util.Vector;
 
 /**
  * @author syeates@gmail.com
@@ -8,12 +10,35 @@
  */
 public class CSVFormatDiviner {
 
+
+    static CSVFormat[] defaults = { 
+        CSVFormat.DEFAULT,
+        CSVFormat.EXCEL,
+        CSVFormat.MYSQL,
+        CSVFormat.RFC4180,        
+    };
+	
+    CSVFormat[] getAllFormats(String delimiters)
+    {
+       	Vector<CSVFormat>formats = new Vector<CSVFormat>();
+    	for (int i=0;i<delimiters.length();i++){
+    		formats.add(CSVFormat.DEFAULT.withDelimiter(delimiters.charAt(i)));
+    	}
+       	Vector<CSVFormat>formats2 = new Vector<CSVFormat>();
+    	for (int i=0;i<formats.size();i++){
+    		formats2.add(formats.elementAt(i).withAllowMissingColumnNames(true));
+    		formats2.add(formats.elementAt(i).withAllowMissingColumnNames(false));
+    	}
+    	
+    
+    	return formats.toArray(new CSVFormat[0]);
+    }
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new CSVFormatDiviner().getAllFormats("|,.;:'!");
 	}
 
 }
