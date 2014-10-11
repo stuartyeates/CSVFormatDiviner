b8b004cf31c8116a18f85dc115ea90e77e00fac6
stuart yeates <syeates@gmail.com>
2014-10-11 22:17:09 +1300

    first commit

---
+++ src/CSVFormatDiviner.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author syeates@gmail.com
+ *
+ */
+public class CSVFormatDiviner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
